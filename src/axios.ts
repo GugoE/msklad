ae788287e9e525b99b7e8a629b1924d261099326
gugo <gaegiazaryan@gmail.com>
2022-02-08 13:55:57 +0400

    add env

--- src/axios.ts
+++ src/axios.ts
@@ -4,16 +4,23 @@ import { Product } from './product.js';
 
 declare module 'axios' {
     interface Axios {
-        getProductById(this: typeof Axios, productId: string):Promise<Product>
+        getProductById(productId: string):Promise<Product>
     }
 }
 
-Axios.prototype.getProductById = function (this: typeof Axios, productId: string):Promise<Product> {
+Axios.prototype.getProductById = function (productId: string):Promise<Product> {
     return moySkladAxios.get<Product>(`/entity/product/${productId}`)
         .then(res => res.data)
 }
-
+console.log(process.env.MOYSKLADTOKEN)
 export const moySkladAxios = axios.create({
-    baseURL:'https://online.moysklad.ru/api/remap/1.2'
+    baseURL:'https://online.moysklad.ru/api/remap/1.2',
+    headers: {
+        Authorization: `Bearer ${process.env.MOYSKLADTOKEN}`
+    }
+})
+
+moySkladAxios.interceptors.response.use(response => response, error => {
+    throw new Error(error.response.data.errors[0].error);
 })
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config({
+    path: './.env'
+});
 import { moySkladAxios } from './axios.js';
 
 moySkladAxios.getProductById('1')
